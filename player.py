bd308f0fbadf0136d0f3b031aa83b245c1808038
Assaf <assaf@razon-family.com>
2021-09-08 23:55:32 +0300

    Change to main.

--- player.py
+++ player.py
@@ -58,171 +58,186 @@ class Player(self):
         canvas.create_image(0, 0, image=self.img, anchor=NW)
         self.fps = 25
 
-    class MenuCom():
+        Click = IntVar()
+        Click.set(frame_list[0])
+        op = ttk.OptionMenu(tab1, Click, *frame_list, command=change_fram_p)
+        op.place(x=10, y=10)
+        vc_click = StringVar()
+        ac_click = StringVar()
+        crf_click = StringVar()
+        audio_click = IntVar()
+        vc_list = [
+            'ac3',
+            'ac3',
+            'copy'
+        ]
+        ac_list = [
+            'libx264',
+            'libx264',
+            'copy'
+        ]
+
+        crf_list = [
+            'copy',
+            'copy',
+            '20',
+            '25'
+        ]
+        audio_list = [
+            0,
+            0,
+            1
+        ]
+        op1 = ttk.OptionMenu(tab1, vc_click, *vc_list, command=change_vc)
+        op1.place(x=63, y=10)
+        op2 = ttk.OptionMenu(tab1, ac_click, *ac_list, command=change_ac)
+        op2.place(x=123, y=10)
+        op3 = ttk.OptionMenu(tab1, crf_click, *crf_list, command=change_crf)
+        op3.place(x=203, y=10)
+        op3 = ttk.OptionMenu(tab1, audio_click, *audio_list, command=change_audio)
+        op3.place(x=10, y=50)
+
+        scale.config(command=set_fram)
+        # video_name is the video being called
+        self.frame = 0
+        i = to_time(frame)
+        l = Label(frame1, text=i, bg='white')
+        l.pack()
+        self.frame_p = 1;
+        frame_list = [
+            0,
+            1,
+            10,
+            20,
+            30,
+            40,
+            100,
+            200
+        ]
+        notebook = ttk.Notebook(root)
+
+        tab1 = ttk.Frame(notebook, width=380, height=380)
+        tab2 = ttk.Frame(notebook, width=380, height=380)
+
+        notebook.add(tab1, text='Tab 1')
+        notebook.add(tab2, text='Tab 2')
+        notebook.place(x=10, y=10)
+        self.dic = {}
+
+        b = ttk.Button(tab2, text='start trim', command=start_trim, state='disabled')
+        b1 = ttk.Button(tab2, text='end trim', command=end_trim, state='disabled')
+        con = ttk.Button(tab2, text='convert', command=convert_, state='disabled')
+        b.place(x=10, y=10)
+        b1.place(x=100, y=10)
+        con.place(x=190, y=10)
+        main_menu = Menu(root)
+        root.config(menu=main_menu)
+        menu = [
+            Menu(main_menu, tearoff=0),
+            Menu(main_menu, tearoff=0),
+            Menu(main_menu, tearoff=0)
+        ]
+        main_menu.add_cascade(label='file', menu=menu[0])
+        main_menu.add_cascade(label='edit', menu=menu[1])
+        main_menu.add_cascade(label='option', menu=menu[2])
+
+        menu[0].add_command(label='file')
+        menu[0].add_command(label='New', command=MenuCom.open_file)
+        menu[0].add_command(label='Exit', command=root.destroy)
+
+        # bind
+        root.bind("<Left>", move_video_left)
+        root.bind("<Right>", move_video_right)
+
+        root.mainloop()
+
+    class MenuCom(self):
         def open_file():
-            global video, length_frame
             root.file_name = filedialog.askopenfilename(initialdir="e:",
                                                         title='select a video file',
                                                         filetypes=(("MKV file", "*.mkv"),
                                                                    ("VLC file", "*.ts"),
                                                                    ("MP4 file", "*.mp4"),
                                                                    ("All file", "*.*")))
-            global inp, a, v, fps
             b.config(state='NORMAL')
-            inp = ffmpeg.input(root.file_name)
-            a = inp['a']
-            v = inp['v']
-            video = cv2.VideoCapture(root.file_name)
+            self.inp = ffmpeg.input(root.file_name)
+            self.a = inp['a']
+            self.v = inp['v']
+            self.video = cv2.VideoCapture(root.file_name)
 
-            fps = video.get(cv2.CAP_PROP_FPS)
+            self.fps = video.get(cv2.CAP_PROP_FPS)
             no_error = True
 
-            global frame
-            frame = 0
+            self.frame = 0
             length_frame = video.get(cv2.CAP_PROP_FRAME_COUNT)
 
-            scale.config(to=length_frame, value=0)
+            scale.config(to=self.length_frame, value=0)
             scale.config(state='NORMAL')
             run_video(0)
 
-    def to_time(fram_):
-        global fps
-        return str(datetime.timedelta(seconds=fram_ / fps))
+    def to_time(self,fram_):
+        return str(datetime.timedelta(seconds=fram_ / self.fps))
 
-    def run_video(fram_num):
-        global img
-        try:
+    def run_video(self,fram_num):
+       try:
             l.config(text=to_time(fram_num))
             video.set(cv2.CAP_PROP_POS_FRAMES, fram_num);  # Where frame_no is the frame you want
-            ret, frame = video.read()  # Read the frame
+            ret, self.frame = video.read()  # Read the frame
 
-            img = ImageTk.PhotoImage(
+            self.img = ImageTk.PhotoImage(
                 Image.fromarray(
                     cv2.cvtColor(
                         cv2.resize(
-                            frame,
+                            self.frame,
                             (560, 400),
                             interpolation=cv2.INTER_NEAREST),
                         cv2.COLOR_BGR2RGB)))
-            canvas.create_image(0, 0, image=img, anchor=NW)
+            canvas.create_image(0, 0, image=self.img, anchor=NW)
         except:
             pass
 
-    scale.config(command=set_fram)
-    # video_name is the video being called
-    global frame
-    frame = 0
-    i = to_time(frame)
-    l = Label(frame1, text=i, bg='white')
-    l.pack()
-    global frame_p
-    frame_p = 1;
-    frame_list = [
-        0,
-        1,
-        10,
-        20,
-        30,
-        40,
-        100,
-        200
-    ]
-    notebook = ttk.Notebook(root)
 
-    tab1 = ttk.Frame(notebook, width=380, height=380)
-    tab2 = ttk.Frame(notebook, width=380, height=380)
+    def change_ac(self,event):
+        self.dic['c:a'] = str(ac_click.get())
 
-    notebook.add(tab1, text='Tab 1')
-    notebook.add(tab2, text='Tab 2')
-    notebook.place(x=10, y=10)
-    global dic
-    dic = {}
+    def change_vc(self,event):
+        self.dic['c:v'] = str(vc_click.get())
 
-    def change_ac(event):
-        dic['c:a'] = str(ac_click.get())
+    def change_crf(self,event):
+        self.dic['crf'] = str(crf_click.get())
 
-    def change_vc(event):
-        dic['c:v'] = str(vc_click.get())
+    def change_fram_p(self,event):
+        self.frame_p = Click.get()
 
-    def change_crf(event):
-        dic['crf'] = str(crf_click.get())
-
-    def change_fram_p(event):
-        global frame_p
-        frame_p = Click.get()
-
-    def change_audio(event):
-        global a
-        a = inp[str('a:' + str(audio_click.get()))]
+    def change_audio(self,event):
+        self.a = inp[str('a:' + str(audio_click.get()))]
         print(str('a:' + str(audio_click.get())))
 
-    Click = IntVar()
-    Click.set(frame_list[0])
-    op = ttk.OptionMenu(tab1, Click, *frame_list, command=change_fram_p)
-    op.place(x=10, y=10)
-    vc_click = StringVar()
-    ac_click = StringVar()
-    crf_click = StringVar()
-    audio_click = IntVar()
-    vc_list = [
-        'ac3',
-        'ac3',
-        'copy'
-    ]
-    ac_list = [
-        'libx264',
-        'libx264',
-        'copy'
-    ]
+    def move_video_right(self,event):
+        if self.frame < self.length_frame:
+            self.frame += self.frame_p
 
-    crf_list = [
-        'copy',
-        'copy',
-        '20',
-        '25'
-    ]
-    audio_list = [
-        0,
-        0,
-        1
-    ]
-    op1 = ttk.OptionMenu(tab1, vc_click, *vc_list, command=change_vc)
-    op1.place(x=63, y=10)
-    op2 = ttk.OptionMenu(tab1, ac_click, *ac_list, command=change_ac)
-    op2.place(x=123, y=10)
-    op3 = ttk.OptionMenu(tab1, crf_click, *crf_list, command=change_crf)
-    op3.place(x=203, y=10)
-    op3 = ttk.OptionMenu(tab1, audio_click, *audio_list, command=change_audio)
-    op3.place(x=10, y=50)
+            self.scale.config(value=frame)
+            self.run_video(self.frame)
 
-    def move_video_right(event):
-        global frame
-        if frame < length_frame:
-            frame += frame_p
+    def move_video_left(self,event):
+        if self.frame > 1:
+            self.frame -= self.frame_p
+            self.scale.config(value=self.frame)
+            run_video(self.frame)
 
-            scale.config(value=frame)
-            run_video(frame)
-
-    def move_video_left(event):
-        global frame
-        if frame > 1:
-            frame -= frame_p
-            scale.config(value=frame)
-            run_video(frame)
-
-    def video():
+    def video(self):
         pass
 
-    def start_trim():
-        global dic
-        dic['ss'] = to_time(scale.get())
+    def start_trim(self):
+        self.dic['ss'] = to_time(scale.get())
         b1.config(state='NORMAL')
 
-    def end_trim():
-        dic['to'] = to_time(scale.get())
+    def end_trim(self):
+        self.dic['to'] = to_time(self.scale.get())
         con.config(state='NORMAL')
 
-    def convert_():
+    def convert_(self):
         global outfile
         out = filedialog.asksaveasfilename(defaultextension=".ts",
                                            initialdir="c:",
@@ -230,36 +245,9 @@ class Player(self):
         b1.config(state='disabled')
         b.config(state='NORMAL')
         con.config(state='disabled')
-        outfile = ffmpeg.output(a, v, out, **dic)
+        self.outfile = ffmpeg.output(a, v, out, **self.dic)
         print(outfile.compile())
         if os.path.isfile(out):
             ffmpeg.run(outfile, overwrite_output=True, capture_stdout=True)
         else:
             ffmpeg.run(outfile)
-
-    b = ttk.Button(tab2, text='start trim', command=start_trim, state='disabled')
-    b1 = ttk.Button(tab2, text='end trim', command=end_trim, state='disabled')
-    con = ttk.Button(tab2, text='convert', command=convert_, state='disabled')
-    b.place(x=10, y=10)
-    b1.place(x=100, y=10)
-    con.place(x=190, y=10)
-    main_menu = Menu(root)
-    root.config(menu=main_menu)
-    menu = [
-        Menu(main_menu, tearoff=0),
-        Menu(main_menu, tearoff=0),
-        Menu(main_menu, tearoff=0)
-    ]
-    main_menu.add_cascade(label='file', menu=menu[0])
-    main_menu.add_cascade(label='edit', menu=menu[1])
-    main_menu.add_cascade(label='option', menu=menu[2])
-
-    menu[0].add_command(label='file')
-    menu[0].add_command(label='New', command=MenuCom.open_file)
-    menu[0].add_command(label='Exit', command=root.destroy)
-
-    # bind
-    root.bind("<Left>", move_video_left)
-    root.bind("<Right>", move_video_right)
-
-    root.mainloop()
