b2c00248bec4dd3d7eb81451ae28c454b2662d5d
AssafR <aU2nQune>
2022-11-07 09:30:16 +0200

    Small unrelated experiment with a basic GUI grid.

---
+++ game_of_life.py
@@ -0,0 +1,59 @@
+import numpy as np
+from tkinter import ttk, Tk, Canvas
+
+RECTANGLE_OFFSET = 10
+WORLD_WIDTH = 25
+WORLD_HEIGHT = 20
+DEFAULT_RECTANGLE_SIZE = 20
+
+
+def random_world(width, height):
+    # Create a random world of width and height consisting of 0/1 in equal probability
+    world = ((0.5 + np.random.rand(width, height)).astype('int8'))
+    return world
+
+
+class Board:
+    def __init__(self):
+        self.root = Tk()
+        self.root.title("Game of Life")
+        self.root.geometry("800x600")
+        self.root.configure(bg='white')
+
+    def board_main(self):
+        self.canvas = Canvas(self.root,  # Canvas on which the grid will be displayed
+                             width=WORLD_WIDTH * DEFAULT_RECTANGLE_SIZE + 2 * RECTANGLE_OFFSET,
+                             height=WORLD_HEIGHT * DEFAULT_RECTANGLE_SIZE + 2 * RECTANGLE_OFFSET,
+                             bg='red')
+        self.canvas.pack()
+        self.btn_next_round = ttk.Button(self.root, text='Next Round', command=self.next_round, state='enabled')
+        self.btn_next_round.place(x=100, y=500)
+        self.next_round()  # Initialize with a first round
+        self.root.mainloop()
+
+    def next_round(self):
+        self.calculate_next_round()
+        self.draw_world_on_canvas(self.world)
+
+    def calculate_next_round(self):
+        # Temporary for development only: Create a new random world
+        self.world = random_world(WORLD_WIDTH, WORLD_HEIGHT)
+
+    def draw_world_on_canvas(self, world):  # Paint the world as a grid on the canvas
+        width, height = world.shape
+        for i in range(width):
+            for j in range(height):
+                if world[i, j] > 0:
+                    fill_color = '#000fff000'  # Green
+                else:
+                    fill_color = '#000000000'  # Black
+                self.canvas.create_rectangle(RECTANGLE_OFFSET + i * DEFAULT_RECTANGLE_SIZE,
+                                             RECTANGLE_OFFSET + j * DEFAULT_RECTANGLE_SIZE,
+                                             RECTANGLE_OFFSET + (i + 1) * DEFAULT_RECTANGLE_SIZE - 1,
+                                             RECTANGLE_OFFSET + (j + 1) * DEFAULT_RECTANGLE_SIZE + -1,
+                                             fill=fill_color)
+
+
+if __name__ == '__main__':
+    my_board = Board()
+    my_board.board_main()
